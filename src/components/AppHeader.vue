5e9b0f1ad5831b7bf277d56f59741e2a9b361707
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 17:08:45 +0100

    feat: header component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,38 @@
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      logo: "/img/logo.png",
+      title: "Yu-Gi-Oh Api",
+    };
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div class="cont">
+    <div class="logo">
+      <img :src="logo" :alt="title" />
+    </div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cont {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  padding: 25px;
+}
+
+.logo {
+  width: 80px;
+}
+
+h1 {
+  font-size: 42px;
+  font-weight: normal;
+}
+</style>
