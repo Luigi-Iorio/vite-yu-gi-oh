ca6dcde4c3c8d302331918c2aea12413b84fd8c9
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 16:55:05 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+import MainSearch from "./MainSearch.vue";
+import MainCards from "./MainCards.vue";
+
+export default {
+  components: {
+    MainSearch,
+    MainCards,
+  },
+};
+</script>
+
+<template>
+  <MainSearch />
+  <MainCards />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,18 @@
+<script>
+import MainCardsNumber from "./MainCardsNumber.vue";
+import MainCard from "./MainCard.vue";
+
+export default {
+  components: {
+    MainCardsNumber,
+    MainCard,
+  },
+};
+</script>
+
+<template>
+  <MainCardsNumber />
+  <MainCard />
+</template>
+
+<style lang="scss" scoped></style>
