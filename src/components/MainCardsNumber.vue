6c1b8e3360b9a6d5a50acf07d4d1e006e30f71b6
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 17:42:29 +0100

    feat: struttura cards number component

--- src/components/MainCardsNumber.vue
+++ src/components/MainCardsNumber.vue
@@ -2,6 +2,16 @@
 export default {};
 </script>
 
-<template></template>
+<template>
+  <div class="container-card-number">
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h3>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container-card-number {
+  background-color: black;
+  color: white;
+  padding: 25px 20px;
+}
+</style>
