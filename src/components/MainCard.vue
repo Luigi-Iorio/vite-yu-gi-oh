0cc256930e15f394a079c3c430834eb60ca6b4b0
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 20:51:10 +0100

    feat: inserimento dati in cards

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -1,28 +1,31 @@
 <script>
-export default {};
+export default {
+  props: ["img", "name", "type"],
+};
 </script>
 
 <template>
   <div class="card">
     <div class="card-image">
-      <img src="" alt="" />
+      <img :src="img" :alt="name" />
     </div>
-    <div class="card-name">name</div>
-    <div class="card-type">type</div>
+    <div class="card-name">{{ name }}</div>
+    <div class="card-type">{{ type }}</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-@use "../assets/scss/partials/variables.scss" as *;
-
 .card {
   display: flex;
   flex-direction: column;
   text-align: center;
   gap: 25px;
-  width: calc((100% / 6 - 25px) + 25px / 6);
-  background-color: $color-main;
 
+  .card-name {
+    font-weight: bold;
+    color: white;
+    text-transform: uppercase;
+  }
   .card-type {
     margin-bottom: 5px;
   }
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,8 +1,14 @@
 <script>
 import MainCardsNumber from "./MainCardsNumber.vue";
 import MainCard from "./MainCard.vue";
+import { store } from "../store";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     MainCardsNumber,
     MainCard,
@@ -13,13 +19,21 @@ export default {
 <template>
   <div class="container-cards">
     <MainCardsNumber />
-    <div class="list-cards">
-      <MainCard />
-    </div>
+    <ul class="list-cards">
+      <li v-for="card in store.yuCards">
+        <MainCard
+          :img="card.card_images[0].image_url"
+          :name="card.name"
+          :type="card.archetype"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
 .container-cards {
   padding: 50px;
   background-color: white;
@@ -30,5 +44,10 @@ export default {
   flex-direction: row;
   gap: 25px;
   flex-wrap: wrap;
+}
+
+li {
+  width: calc((100% / 5 - 25px) + 25px / 5);
+  background-color: $color-main;
 }
 </style>
