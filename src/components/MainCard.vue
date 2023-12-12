740272702f0f56422213ea6dfd9112f3996325ff
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 18:12:52 +0100

    feat: struttura card

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -2,6 +2,29 @@
 export default {};
 </script>
 
-<template></template>
+<template>
+  <div class="card">
+    <div class="card-image">
+      <img src="" alt="" />
+    </div>
+    <div class="card-name">name</div>
+    <div class="card-type">type</div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
+.card {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 25px;
+  width: calc((100% / 6 - 25px) + 25px / 6);
+  background-color: $color-main;
+
+  .card-type {
+    margin-bottom: 5px;
+  }
+}
+</style>
