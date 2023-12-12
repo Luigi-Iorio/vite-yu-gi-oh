60bf1912694596084d2935d4381cd668f9d85ba0
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-12 20:47:29 +0100

    fix: import store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import { reactive } from "Vue";
+import { reactive } from "vue";
 
 export const store = reactive({
   yuCards: [],
-  apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+  apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0",
 });
