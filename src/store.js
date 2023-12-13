2735f89a0beb1bd4619099bbdb6f50c9c08a98b7
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-13 15:10:05 +0100

    feat: api e array per l'archetipo delle card

--- src/store.js
+++ src/store.js
@@ -3,4 +3,6 @@ import { reactive } from "vue";
 export const store = reactive({
   yuCards: [],
   apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0",
+  yuArchetype: [],
+  apiArchetypeUrl: "https://db.ygoprodeck.com/api/v7/archetypes.php",
 });
